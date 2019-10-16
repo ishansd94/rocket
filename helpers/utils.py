25d188eb4841364787e5b82f8cbbc73618291488
luqmanMohammed <mohammed.luqman@pearson.com>
2019-10-17 05:19:49 +0530

    Add current in progress write to source method

--- helpers/utils.py
+++ helpers/utils.py
@@ -12,3 +12,7 @@ def past_in_progress_tickets(filepath="../past_in_progress/COPS.json"):
     with open(filepath) as past_progress_json:
         data = json.load(past_progress_json)
         return data
+
+def write_in_progress_tickets(data,writepath="../past_in_progress/COPS.json"):
+    with open(writepath,"w") as write_json:
+        json.dump(data,write_json)
